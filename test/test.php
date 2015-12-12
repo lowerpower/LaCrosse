0054c29b68c8aa76cb9197f651967a9e3bac1558
Drew Stephens <drew.stephens@aexp.com>
2015-12-11 19:14:13 -0500

    Stub test of WUdata

--- test/test.php
+++ test/test.php
@@ -1,6 +1,12 @@
 <?php
 function getallheaders() {
-    return "";
+    return array();
 }
 
 require_once('request.breq');
+
+$packet = file_get_contents('test/packets/197-byte-weather.dump');
+
+$nybs = post2nyb($packet);
+$wuData = assembleWUData($nybs, 1);
+echo "$wuData\n";
