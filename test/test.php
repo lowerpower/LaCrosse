58cad183f5672dce846dd6ec2f7a589929f74a6e
Drew Stephens <drew.stephens@aexp.com>
2015-12-11 20:33:00 -0500

    Use PHPUnit

--- test/test.php
+++ test/test.php
@@ -2,11 +2,21 @@
 function getallheaders() {
     return array();
 }
-
 require_once('request.breq');
 
-$packet = file_get_contents('test/packets/197-byte-weather.dump');
+class RequestTest extends PHPUnit_Framework_TestCase
+{
 
-$nybs = post2nyb($packet);
-$wuData = assembleWUData($nybs, 1);
-echo "$wuData\n";
+    public function testCanBeNegated()
+    {
+        $packet = file_get_contents('test/packets/197-byte-weather.dump');
+
+        $nybs = post2nyb($packet);
+        $wuData = assembleWUData($nybs, 1);
+
+        $expected = "&tempf=52.88&humidity=82&dewptf=48.86&indoortempf=68.36&indoorhumidity=59&windspeedmph=0.00&windgustmph=0.00&winddir=270&baromin=29.91&rainin=0.00&dailyrainin=0.00&softwaretype=phpLaxWeatherBro";
+        $this->assertEquals($expected, $wuData);
+    }
+}
+
+
